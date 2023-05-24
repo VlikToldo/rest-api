03d6341178ef29c20616f478aa8a015ce6a62dbb
VlikToldo <108451651+VlikToldo@users.noreply.github.com>
2023-05-24 23:06:58 +0300

    add DB_HOST to config

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const app = require('./app');
 
-const DB_HOST = 'mongodb+srv://Valik:sEqsMTVjoSOwKBXK@cluster0.t6e8zmf.mongodb.net/my_contacts?retryWrites=true&w=majority';
+const {DB_HOST} = require('./config')
+
 mongoose.connect(DB_HOST)
   .then(()=> {
     app.listen(3000)
